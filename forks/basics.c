1d6612604cceb4b45dbf04b57b0f3366fbac2177
jakobthesheep <jakobstanleywarth@gmail.com>
2021-03-07 12:53:46 +0100

    add Folder makefile

--- forks/basics.c
+++ forks/basics.c
@@ -1,15 +1,17 @@
 #include <stdio.h> 
 #include <sys/types.h> 
 #include <unistd.h> 
+
 void forkexample() 
 { 
+    int forkVal = fork();
     // child process because return value zero 
-    if (fork() == 0) 
-        printf("Hello from Child!\n"); 
-  
+    if (forkVal == 0) 
+        printf("Hello from Child! Fork value: %d\n", forkVal); 
+
     // parent process because return value non-zero. 
     else
-        printf("Hello from Parent!\n"); 
+        printf("Hello from Parent! Fork value: %d\n", forkVal); 
 } 
 int main() 
 { 
