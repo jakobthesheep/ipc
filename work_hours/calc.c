aae58615aea0a563fc44f9e1f42ca4310f055f63
jakobthesheep <jakobstanleywarth@gmail.com>
2021-03-09 22:41:24 +0100

    Small changes to work_hours

--- work_hours/calc.c
+++ work_hours/calc.c
@@ -2,20 +2,28 @@
 #include <sys/types.h> 
 #include <unistd.h>
 #include <string.h> 
+#include <stdlib.h>
 
 
 int main(int argc, char * argv []) 
 { 
     printf("%d\n", argc);
-    char * start;
-    char * end;
+    int start_hour;
+    int start_min;
+    int end_hour;
+    int end_min;
 
-    if (argc == 3 && strlen(argv[1]) == 4 && strlen(argv[2]) == 4) {
-        start = argv[1];
-        end = argv[2];
-        printf("%s, %s\n", start, end);
+    if (argc == 5) {
+        start_hour = atoi(argv[1]);
+        start_min = atoi(argv[2]);
+        end_hour = atoi(argv[3]);
+        end_min = atoi(argv[4]);
+
+        printf("%d, %d, %d, %d\n", start_hour, start_min, end_hour, end_min);
+        printf("%d", end_hour+12-start_hour);
         return 0; 
     } else {
+        printf("Something went wrong...");
         return 1;
     }
 } 
